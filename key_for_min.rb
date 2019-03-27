a9477f7d3922a66babe28461576a60cd1fb9f65c
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-27 18:03:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
     counter_key = nil  
-    counter_value = name_hash["key"] 
+    counter_value = name_hash[nil] 
   name_hash.collect do |key , value| 
     if value < counter_value 
       counter_value = value 
